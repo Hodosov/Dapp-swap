2dc8965127a6a05281ab0bab61e67536ee848a75
Hodosov Yaroslav <hodosov.y@mail.ru>
2021-11-03 20:57:38 +0300

    connect to Blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+import React, { Component } from "react";
+import Web3 from "web3";
+import logo from "../logo.png";
+import "./App.css";
+import NavBar from "./NavBar";
 
 class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const [account] = await web3.eth.getAccounts()
+    this.setState({account})
+    const ethBallance = await web3.eth.getBalance(account)
+    this.setState({ethBallance})
+
+  }
+
+  async loadWeb3() {
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+      }
+      else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+      } else {
+        console.log(
+          "Non-Ethereum browser detected. You should consider trying MetaMask!"
+        );
+      }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: "",
+      ethBallance: "0"
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <NavBar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
@@ -37,7 +66,10 @@ class App extends Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  LEARN BLOCKCHAIN <u><b>NOW!! </b></u>
+                  LEARN BLOCKCHAIN{" "}
+                  <u>
+                    <b>NOW!! </b>
+                  </u>
                 </a>
               </div>
             </main>
