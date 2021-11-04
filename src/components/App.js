6ee454e6289d10987a6d0f167873c525c921fbc8
Hodosov Yaroslav <hodosov.y@mail.ru>
2021-11-04 23:51:28 +0300

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ const App = () => {
 
   useEffect(async () => {
     await loadBlockchainData();
-  }, [buyTokens]) 
+  }, []) 
 
   async function loadBlockchainData() {
     const web3 = window.web3;
